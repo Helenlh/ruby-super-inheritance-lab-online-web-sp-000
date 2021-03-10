e9281330c2598eac7e2a6f50c735b1ad6db5f785
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-10 04:16:37 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,13 +1,21 @@
 require 'pry'
 
+<<<<<<< HEAD
 class ChattyStudent < Student 
   
   def hello 
     super
+=======
+class Chatty_student
+  
+  def hello 
+    super(message)
+>>>>>>> 4467156933adf8794311ba9b9d15adc6323fb829
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
   def raise_hand 
+<<<<<<< HEAD
      super
      super
      super
@@ -27,3 +35,12 @@ end
 #     @in_class = true
 #   end
 # end
+=======
+    times do
+    puts "TIMES: 4"
+      
+    
+      
+    puts "TIMES: 4"
+end 
+>>>>>>> 4467156933adf8794311ba9b9d15adc6323fb829
--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,19 @@ require 'pry'
 class Student
   
   
+<<<<<<< HEAD
   def hello
     puts "Hey there! I'm so excited to learn stuff."
   end 
   
   def raise_hand
+=======
+  def hello(message)
+    puts "Hey there!  I'm so excited to learn stuff."
+  end 
+  
+  def raise_hand(phrase)
+>>>>>>> 4467156933adf8794311ba9b9d15adc6323fb829
     puts "Pick me!"
   end  
 end 
