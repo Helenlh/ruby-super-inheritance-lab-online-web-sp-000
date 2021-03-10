1741bf1c3bfe21c564451df134dac6d859f687dd
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-10 04:20:47 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,16 +1,9 @@
 require 'pry'
 
-<<<<<<< HEAD
 class ChattyStudent < Student 
   
   def hello 
     super
-=======
-class Chatty_student
-  
-  def hello 
-    super(message)
->>>>>>> 4467156933adf8794311ba9b9d15adc6323fb829
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
@@ -27,4 +20,3 @@ class Chatty_student
     puts "Pick me!"
   end 
 end 
-
